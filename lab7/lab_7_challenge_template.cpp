4d723bab18445d0b528b06f6053ec4778575f890
Joel Macias <macias22@unlv.nevada.edu>
2016-10-11 16:05:08 -0700

    added lab7 notes and challenges

---
+++ lab7/lab_7_challenge_template.cpp
@@ -0,0 +1,20 @@
+/*
+  CS135L Lab 7 Programming Challenge 1 Template
+ 
+ Write a C++ program that prints the numbers from 1 to 100. But for multiples of three print "Fizz" instead
+ of the number, for multiples of five print "Buzz", and for multiples of both three and five print "FizzBuzz"
+ 
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+
+    return 0;
+}
+
+
+
